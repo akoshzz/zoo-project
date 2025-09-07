15d284cc8b0a4233bfa49e9960656f5bc3ca2ca8
akoshzz <aknazarsovetbek@gmail.com>
2025-09-07 13:25:06 +0600

    feat: i made classes eat food

--- akoshzz.py
+++ akoshzz.py
@@ -31,3 +31,34 @@ elephant = Elephant("Batyr", 10)
 lion.make_sound()      
 monkey.make_sound()   
 elephant.make_sound() 
+
+class Lion(Animal):
+    def make_sound(self):
+        print("Roar! ")
+
+    def feed(self):
+        print(f"{self.name} eats meat.")
+
+
+class Monkey(Animal):
+    def make_sound(self):
+        print("Ooh-ooh!")
+
+    def feed(self):
+        print(f"{self.name} eats banana.")
+
+
+class Elephant(Animal):
+    def make_sound(self):
+        print("Trumpet!")
+
+    def feed(self):
+        print(f"{self.name} eats grass.")
+
+lion = Lion("Aknazar", 5)
+monkey = Monkey("Yerassyl", 3)
+elephant = Elephant("Batyr", 10)
+
+lion.feed()     
+monkey.feed()    
+elephant.feed() 
